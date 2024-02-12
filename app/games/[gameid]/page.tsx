357ab183b7ef28c4f192e25b79905a5c0d80cd8a
Anthony Demott <java152001@gmail.com>
2024-02-12 13:17:24 -0700

    Added styling to board game details page"

--- app/games/[gameid]/page.tsx
+++ app/games/[gameid]/page.tsx
@@ -1,5 +1,6 @@
 import { client } from "@/app/utils/configSanity";
-import { Group, Stack, Title, Text, Image, Container } from '@mantine/core';
+import Link from "next/link";
+import { Group, Stack, Title, Text, Image, Container, Box, Button } from '@mantine/core';
 
 interface IGame {
     _id: string;
@@ -24,16 +25,57 @@ export default async function Page({ params }: { params: { gameid: string }}) {
 
     const game = (await getGame(params.gameid)) as IGame[];
 
+    const description = game[0].description.split('&#10;').join('\n');
+
     return (
-        <Container>
-            <Group>
+        <Container mt='75' className='bg-forest-green rounded-xl' p="xl">
+            <Group 
+                justify="space-evenly" 
+                mb='xl'
+                classNames={{
+                    root: 'nowrap'
+                }}>
+                <Image
+                    maw='35%' 
+                    src={game[0].image}
+                    alt={game[0].name}
+                    height={250}
+                    width={250}
+                />
                 <Stack>
-                    <Group>
-                        {game[0].categories.map((category) => <Text>{category}</Text>)}
-                    </Group>
+                    <Stack>
+                        <Box m='auto'>
+                            <Button><Link href='/mycollection'>Back to your collection</Link></Button>
+                        </Box>
+                        <Group
+                            justify="center"
+                            classNames={{
+                                root: 'nowrap'
+                            }}
+                        >
+                            {game[0].categories.map((category) => <Text>{category}</Text>)}
+                        </Group>
+                        <Box>
+                            <Title ta="center">{game[0].name}</Title>
+                        </Box>
+                        <Box>
+                            <Group justify="center">
+                                <Text>Play Time: {game[0].playTime}</Text>
+                                <Text>Players: {game[0].minPlayers} - {game[0].maxPlayers}</Text>
+                                <Text>Age: {game[0].minAge}</Text>
+                            </Group>
+                        </Box>
+                        <Box>
+                            <Group gap='0' justify="center">
+                                {game[0].mechanics.map((mechanic) => <Text size="sm" mr='md'>{mechanic}</Text>)}
+                            </Group>
+                        </Box>
+                    </Stack>
                 </Stack>
             </Group>
-            <h1>The id is {params.gameid}</h1>
+            <Box>
+                <Text size='sm' styles={{ root: { whiteSpace: 'pre-wrap' }}}>{description}</Text>
+            </Box>
         </Container>
     )
 }
